ff96d7c9a47d6aa7f5d3449fb57af9bc25b2c6ce
Ting <hsiaoting1003@gmail.com>
2025-04-09 18:12:44 +0800

    weekly 1 - 設定 User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,20 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    // 允許批量填充的欄位
     protected $fillable = [
         'name',
         'email',
         'password',
+        'is_admin',
+        'age',
     ];
 
     /**
@@ -28,9 +33,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    // 隱藏欄位
     protected $hidden = [
+        // 回傳的時候不會顯示
         'password',
-        'remember_token',
     ];
 
     /**
@@ -38,7 +44,8 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
+    // 自動轉換資料格式
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean',
     ];
 }
