c45b518752441cbc2b35452ff4e62e6031a4733f
Ting <hsiaoting1003@gmail.com>
2025-04-09 18:09:58 +0800

    weekly 1 - 設定 users api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// 回傳所有使⽤者
+Route::get('/users', [UserController::class, 'index']);
+
+// 新增使⽤者（驗證 name & email ）
+Route::post('/users', [UserController::class, 'store']);
+
+// 查詢特定使⽤者
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+// 整筆更新使⽤者
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+// 刪除使⽤者
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->middleware('admin');
